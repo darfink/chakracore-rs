67b51327d7658e846d1c7c335decd78e05f70cc7
Elliott Linder <elliott.darfink@gmail.com>
2018-08-16 18:38:47 +0200

    feat: update ChakraCore version → 1.10.2

--- chakracore-sys/build.rs
+++ chakracore-sys/build.rs
@@ -8,7 +8,7 @@ use std::path::{Path, PathBuf};
 
 const LIBRARY: &'static str = "ChakraCore";
 const REPOSITORY: &'static str = "https://github.com/Microsoft/ChakraCore.git";
-const VERSION: &'static str = "1.8.1";
+const VERSION: &'static str = "1.10.2";
 const LIBS: [(&'static str, &'static str); 1] = [
     ("lib", "ChakraCoreStatic"),
 ];
